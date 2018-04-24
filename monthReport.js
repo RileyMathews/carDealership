5e3188c77f059997ee896e8c4b29122e137940e7
Riley Mathews <rileymathews80@gmail.com>
2018-04-24 09:37:20 -0500

    finished best month of sales report

--- monthReport.js
+++ monthReport.js
@@ -1,7 +1,23 @@
 //generate report on which month sold the most cars
 
 const monthReport = () => {
-    month = transactions[0].purchase_date.slice(5, 7)
+    let bestMonthIndex = 0
+    let bestMonth = ''
+    let monthSales = [0,0,0,0,0,0,0,0,0,0,0,0]
+    const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    transactions.forEach(transaction => {
+        month = parseInt(transaction.purchase_date.slice(5, 7))
+        monthSales[month - 1] += 1
+    });
+    lastMonth = 0
+    monthSales.map((value, index) => {
+        if (value > lastMonth) {
+            bestMonthIndex = index
+            lastMonth = value
+        } 
+    })
+    bestMonth = months[bestMonthIndex]
+    console.log(`The best month of sales was ${bestMonth}`)
 }
 
 monthReport()
