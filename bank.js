1834245a4b58bb4805049a1936ebef00e2b4e29b
Riley Mathews <rileymathews80@gmail.com>
2018-04-24 10:13:26 -0500

    finished bank report

---
+++ bank.js
@@ -0,0 +1,27 @@
+//app to report the most used bank
+const bankReport = () => {
+    mostUsedBank = ''
+    const banks = []
+    let bankUsed = []
+    transactions.map(sale => {
+        let name = sale.credit.credit_provider
+        if (!banks.includes(name)) {
+            banks.push(name)
+            bankUsed[banks.indexOf(name)] = 1
+        } else {
+            bankUsed[banks.indexOf(name)] += 1
+        }
+    })
+    let mostSold = 0
+    let mostUsedBankIndex
+    bankUsed.map((current, index) => {
+        if (current > mostSold) {
+            mostSold = current
+            mostUsedBankIndex = index
+        }
+    })
+    mostUsedBank = banks[mostUsedBankIndex]
+    console.log(`${mostUsedBank} was the most used bank`)
+}
+
+bankReport()
