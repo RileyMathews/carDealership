49ba5c71c3479ad77079f324abcb1c937ad6ec76
Riley Mathews <rileymathews80@gmail.com>
2018-04-24 10:05:50 -0500

    finished employee profit report

---
+++ employeeProfit.js
@@ -0,0 +1,27 @@
+//app to report which employee had the most profit
+const employeeProfitReport = () => {
+    mostEmployeeProfit = ''
+    const employees = []
+    let employeeProfit = []
+    transactions.map(sale => {
+        let name = sale.sales_agent.first_name
+        if (!employees.includes(name)) {
+            employees.push(name)
+            employeeProfit[employees.indexOf(name)] = sale.gross_profit
+        } else {
+            employeeProfit[employees.indexOf(name)] += sale.gross_profit
+        }
+    })
+    let bestProfit = 0
+    let mostSellingEmployeeIndex
+    employeeProfit.map((current, index) => {
+        if (current > bestProfit) {
+            bestProfit = current
+            mostSellingEmployeeIndex = index
+        }
+    })
+    mostEmployeeProfit = employees[mostSellingEmployeeIndex]
+    console.log(`${mostEmployeeProfit} had the most profit`)
+}
+
+employeeProfitReport()
