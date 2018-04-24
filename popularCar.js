339a97aa7ec3c0c6033e1847d6d4cfc021733755
Riley Mathews <rileymathews80@gmail.com>
2018-04-24 10:09:59 -0500

    finished most popular car report

---
+++ popularCar.js
@@ -0,0 +1,27 @@
+//app to report most popular car sold
+const popularCarReport = () => {
+    mostPopularCar = ''
+    const cars = []
+    let carSales = []
+    transactions.map(sale => {
+        let car = sale.vehicle.model
+        if (!cars.includes(car)) {
+            cars.push(car)
+            carSales[cars.indexOf(car)] = 1
+        } else {
+            carSales[cars.indexOf(car)] += 1
+        }
+    })
+    let mostSold = 0
+    let mostPopularCarIndex
+    carSales.map((current, index) => {
+        if (current > mostSold) {
+            mostSold = current
+            mostPopularCarIndex = index
+        }
+    })
+    mostPopularCar = cars[mostPopularCarIndex]
+    console.log(`${mostPopularCar} was the most popular car`)
+}
+
+popularCarReport()
